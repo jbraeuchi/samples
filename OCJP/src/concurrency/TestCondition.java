940e50a73be8098e15fe68534dc1e8a294c2df0c
jakob <jbraeuchi@gmail.com>
2015-10-14 18:57:27 +0200

    test

--- OCJP/src/concurrency/TestCondition.java
+++ OCJP/src/concurrency/TestCondition.java
@@ -17,20 +17,20 @@ public class TestCondition {
     final Condition notEmpty = lock.newCondition();
 
     final Object[] items = new Object[100];
-    int putptr, takeptr, size;
+    int putptr, takeptr, count;
 
     public void put(Object x) throws InterruptedException {
         count_put.incrementAndGet();
 
         lock.lock();
         try {
-            while (size == items.length)
+            while (count == items.length)
                 notFull.await();
             items[putptr] = x;
             if (++putptr == items.length) {
                 putptr = 0;
             }
-            ++size;
+            ++count;
             notEmpty.signal();
         } finally {
             lock.unlock();
@@ -42,13 +42,13 @@ public class TestCondition {
 
         lock.lock();
         try {
-            while (size == 0)
+            while (count == 0)
                 notEmpty.await();
             Object x = items[takeptr];
             if (++takeptr == items.length) {
                 takeptr = 0;
             }
-            --size;
+            --count;
             notFull.signal();
             return x;
         } finally {
@@ -66,7 +66,7 @@ public class TestCondition {
                     while (true) {
                         tc.put(new Object());
                         sleep(100);
-                        System.out.println(tc.size);
+                        System.out.println(tc.count);
                     }
                 } catch (InterruptedException e) {
                 }
@@ -82,7 +82,7 @@ public class TestCondition {
                     while (true) {
                         tc.take();
                         sleep(110);
-                        System.out.println(tc.size);
+                        System.out.println(tc.count);
                     }
                 } catch (InterruptedException e) {
                 }
