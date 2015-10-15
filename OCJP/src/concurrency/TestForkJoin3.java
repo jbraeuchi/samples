f524006c51b7a63f3cc3fb1260aa523db6d2abdf
jakob <jbraeuchi@gmail.com>
2015-10-15 18:09:11 +0200

    test

--- OCJP/src/concurrency/TestForkJoin3.java
+++ OCJP/src/concurrency/TestForkJoin3.java
@@ -6,19 +6,19 @@ import java.util.concurrent.RecursiveAction;
 /**
  * Zahlen im Array verdoppeln
  */
-public class TestForkJoin2 extends RecursiveAction {
+public class TestForkJoin3 extends RecursiveAction {
 
     final int[] data;
     final int lo;
     final int hi;
 
 
-    TestForkJoin2(int[] data) {
+    TestForkJoin3(int[] data) {
         this(data, 1, data.length);
-    }
+     }
 
 
-    TestForkJoin2(int[] data, int lo, int hi) {
+    TestForkJoin3(int[] data, int lo, int hi) {
         System.out.println("> " + lo + " - " + hi);
         this.data = data;
         this.lo = lo;
@@ -36,22 +36,25 @@ public class TestForkJoin2 extends RecursiveAction {
         //        mid = (lo + hi) >>> 1;
         System.out.println(lo + " - " + hi + " : " + mid);
 
-        TestForkJoin2 f1 = new TestForkJoin2(data, lo, mid);
-        TestForkJoin2 f2 = new TestForkJoin2(data, mid + 1, hi);
-        invokeAll(f1, f2);
+        TestForkJoin3 f1 = new TestForkJoin3(data, lo, mid);
+        TestForkJoin3 f2 = new TestForkJoin3(data, mid + 1, hi);
+        // fork, compute, join !
+        f1.fork();
+        f2.compute();
+        f1.join();
     }
 
     public static void main(String args[]) {
         int data[] = new int[]{1, 12, 3, 4, 25, 6, 37, 8, 9, 10};
 //        data = new int[500];
 //        Arrays.fill(data, 42);
-        TestForkJoin2 f = new TestForkJoin2(data);
+        TestForkJoin3 f = new TestForkJoin3(data);
 
         long t1 = System.currentTimeMillis();
         f.compute();
         long t2 = System.currentTimeMillis();
 
-        String str = String.format("TestForkJoin2 : %s, time: %d ms", Arrays.toString(data), (t2 - t1));
+        String str = String.format("TestForkJoin3 : %s, time: %d ms",  Arrays.toString(data), (t2 - t1));
         System.out.println(str);
     }
 
