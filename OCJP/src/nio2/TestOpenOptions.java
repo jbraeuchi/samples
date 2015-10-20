7e20f0080dbf14c132dd2eba32626c0e072e223d
jakob <jbraeuchi@gmail.com>
2015-10-20 10:37:56 +0200

    test

--- OCJP/src/nio2/TestOpenOptions.java
+++ OCJP/src/nio2/TestOpenOptions.java
@@ -8,8 +8,7 @@ import java.nio.file.*;
  * Created by jakob on 18.10.2015.
  */
 public class TestOpenOptions {
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String args[]) throws Exception {
         // will NOT overwrite existing contents
         OpenOption[] opts1 = new OpenOption[]{StandardOpenOption.CREATE};
 
@@ -22,17 +21,19 @@ public class TestOpenOptions {
         // will overwrite existing contents
         OpenOption[] opts4 = new OpenOption[]{StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING};
 
-        // will delete on close
-        OpenOption[] opts5 = new OpenOption[]{StandardOpenOption.CREATE, StandardOpenOption.DELETE_ON_CLOSE};
+        // will append new writes and delete on close
+        OpenOption[] opts5 = new OpenOption[]{StandardOpenOption.WRITE, StandardOpenOption.APPEND, StandardOpenOption.DELETE_ON_CLOSE};
 
         Path writeFile = Paths.get("c:\\temp\\test.txt");
-        BufferedWriter br = Files.newBufferedWriter(writeFile, Charset.forName("UTF-8"),opts4);
-        br.write("This text file is created using Path API 1");
+        BufferedWriter br = Files.newBufferedWriter(writeFile, Charset.forName("UTF-8"), opts4);
+        br.write("This text file is created using Path API");
         br.flush();
         br.close();
 
-        Path deleteFile = Paths.get("c:\\temp\\delete.txt");
-        BufferedWriter br2 = Files.newBufferedWriter(deleteFile, Charset.forName("UTF-8"),opts5);
+        Path deleteFile = writeFile.resolveSibling("delete.txt");  // -> c:/temp/delete.txt
+        Files.copy(writeFile, deleteFile);
+
+        BufferedWriter br2 = Files.newBufferedWriter(deleteFile, opts5);
         br2.write("File will be deleted when closed");
         br2.flush();
         br2.close();
