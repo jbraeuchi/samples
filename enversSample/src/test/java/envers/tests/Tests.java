429be9f1a40602368e306375f9edf87766ecc776
jakob <jbraeuchi@gmail.com>
2015-10-22 20:12:47 +0200

    added audit query

--- enversSample/src/test/java/envers/tests/Tests.java
+++ enversSample/src/test/java/envers/tests/Tests.java
@@ -25,34 +25,34 @@ public class Tests {
         EntityManager em = emf.createEntityManager();
 
         // Mr. Bond wird erfasst
-        EnvPerson p1 = new EnvPerson();        
+        EnvPerson p1 = new EnvPerson();
         p1.setName("Bond");
         p1.setVorname("James");
         p1.setAdresse("Somewhere in the Bahamas");
-  
+
         EntityTransaction tx1 = em.getTransaction();
         tx1.begin();
         em.persist(p1);
         tx1.commit();           // A: erste Revision
-        
+
         long id = p1.getId();
 
         // Mr. Bond reist weiter
         EnvPerson p2 = em.find(EnvPerson.class, id);
         p2.setAdresse("Somewhere in the Fijis");
-        
+
         EntityTransaction tx2 = em.getTransaction();
         tx2.begin();
         em.merge(p2);
         tx2.commit();           // B: zweite Revision
-        
+
         // Mr. Bond wird gelöscht (Sicherheit geht vor)      
         EnvPerson p3 = em.find(EnvPerson.class, id);
         EntityTransaction tx3 = em.getTransaction();
         tx3.begin();
         em.remove(p3);
         tx3.commit();           // C: dritte Revision
-      
+
         AuditReader ar = AuditReaderFactory.get(em);
         List<Number> revisions = ar.getRevisions(EnvPerson.class, id);  // alle Revisions
         EnvPerson p4 = ar.find(EnvPerson.class, id, revisions.get(0));
@@ -60,44 +60,44 @@ public class Tests {
 
         em.close();
     }
-    
+
     @Test
     public void testAllEntities() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("TEST");
         EntityManager em = emf.createEntityManager();
 
         // Mr. Bond wird erfasst
-        EnvPerson p1 = new EnvPerson();        
+        EnvPerson p1 = new EnvPerson();
         p1.setName("Bond");
         p1.setVorname("James");
         p1.setAdresse("Somewhere in the Bahamas");
-  
+
         EntityTransaction tx1 = em.getTransaction();
         tx1.begin();
         em.persist(p1);
         tx1.commit();           // A: erste Revision
-        
+
         long id = p1.getId();
 
         // Mr. Bond reist weiter, Mr. Holmes wird erfasst, IBM wird erfasst
         EnvPerson p2 = em.find(EnvPerson.class, id);
         p2.setAdresse("Somewhere in the Fijis");
-        
-        EnvPerson px = new EnvPerson();        
+
+        EnvPerson px = new EnvPerson();
         px.setName("Holmes");
         px.setVorname("Sherlock");
         px.setAdresse("221b Baker Street, London");
-        
+
         EnvCompany ibm = new EnvCompany();
         ibm.setName("IBM");
-        
+
         EntityTransaction tx2 = em.getTransaction();
         tx2.begin();
         em.persist(p2);
         em.persist(px);
         em.persist(ibm);
         tx2.commit();           // B: zweite Revision
-        
+
         AuditReader ar = AuditReaderFactory.get(em);
         List<Number> revisions = ar.getRevisions(EnvPerson.class, id);  // alle Revisions
 
