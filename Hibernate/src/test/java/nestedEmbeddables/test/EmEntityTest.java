c51fbe526098bce3475ed989457260f31dbb0307
jakob braeuchi <jbraeuchi@gmx.ch>
2020-03-23 20:32:52 +0100

    hibernate

--- Hibernate/src/test/java/nestedEmbeddables/test/EmEntityTest.java
+++ Hibernate/src/test/java/nestedEmbeddables/test/EmEntityTest.java
@@ -48,4 +48,38 @@ public class EmEntityTest extends TestCase {
         em.persist(e1);
         tx1.commit();
     }
+
+    public void testEmbedded_udate() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TEST");
+        EntityManager em = emf.createEntityManager();
+
+        EmEmbeddableOne o1 = new EmEmbeddableOne();
+        o1.setName("Embeddable One");
+        o1.setDate(LocalDate.now());
+        EmEmbeddableOne o2 = new EmEmbeddableOne();
+        o2.setName("Embeddable Two");
+        o2.setDate(LocalDate.now());
+
+        EmEntity e1 = new EmEntity();
+        e1.setName("Entity 1");
+        e1.setBirthdate(LocalDate.now());
+        e1.getManyOnes().add(o1);
+        e1.getManyOnes().add(o2);
+
+        EntityTransaction tx1 = em.getTransaction();
+        tx1.begin();
+        em.persist(e1);
+        tx1.commit();
+
+        em.clear();
+
+        EntityTransaction tx2 = em.getTransaction();
+        tx2.begin();
+        EmEntity e1db = em.find(EmEntity.class, e1.getId());
+        EmEmbeddableOne o3 = new EmEmbeddableOne();
+        o3.setName("Embeddable Three");
+        o3.setDate(LocalDate.now());
+        e1db.getManyOnes().add(o2);
+        tx2.commit();
+    }
 }
