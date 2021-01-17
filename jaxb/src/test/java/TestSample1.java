cd782e9a6ed3054df3043fba8d43932291a5a4e3
jakob braeuchi <jbraeuchi@gmx.ch>
2021-01-17 17:27:07 +0100

    jaxb xsd to java

--- jaxb/src/test/java/TestSample1.java
+++ jaxb/src/test/java/TestSample1.java
@@ -2,12 +2,31 @@ import ch.brj.jaxb.xsd.sample1.ObjectFactory;
 import ch.brj.jaxb.xsd.sample1.Shiporder;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
 public class TestSample1 {
 
     @Test
-    void test() {
+    void test() throws JAXBException {
         ObjectFactory of = new ObjectFactory();
+
+        Shiporder.Shipto shipTo = of.createShiporderShipto();
+        shipTo.setCity("The City");
         Shiporder so = of.createShiporder();
+        so.setOrderperson("The Person");
+        so.setShipto(shipTo);
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(Shiporder.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        StringWriter writer = new StringWriter();
+        jaxbMarshaller.marshal(so, writer);
+
+        System.out.println(writer.toString());
     }
 
 }
--- jaxb/src/test/java/TestSample2.java
+++ jaxb/src/test/java/TestSample2.java
@@ -1,16 +1,37 @@
 
+import ch.brj.jaxb.xsd.sample2.Ingredients;
 import ch.brj.jaxb.xsd.sample2.ObjectFactory;
-import ch.brj.jaxb.xsd.sample2.PurchaseOrderType;
+
+import ch.brj.jaxb.xsd.sample2.Recipe;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
 
 public class TestSample2 {
 
     @Test
-    void test() {
+    void test() throws JAXBException {
         ObjectFactory of = new ObjectFactory();
-        PurchaseOrderType po = of.createPurchaseOrderType();
-    }
 
+        Ingredients ingredients = of.createIngredients();
+        ingredients.getItem().add("Ingredient 1");
+        ingredients.getItem().add("Ingredient 2");
+
+        Recipe recipe = of.createRecipe();
+        recipe.setRecipeName("The Name");
+        recipe.setMeal("The Meal");
+        recipe.setIngredients(ingredients);
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(Recipe.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        StringWriter writer = new StringWriter();
+        jaxbMarshaller.marshal(recipe, writer);
+
+        System.out.println(writer.toString());
+    }
 }
